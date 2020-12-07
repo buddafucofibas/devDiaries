8062097d844132695e90065aa419611c3efb4252
Dorian Edwards <dorian.m.edwards@gmail.com>
2020-12-06 23:51:36 -0500

    added middleware to check if user is author; added id parameter to routes; finished new Post functionality

--- routes/post.js
+++ routes/post.js
@@ -1,18 +1,36 @@
 const express = require('express')
-const { route } = require('.')
+const Post = require('../models/Post')
+const Author = require('../models/Author')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+const checkIsOwner = (req, res, next) => {
+  if (req.params.id !== req.session.user_id) {
+    return res.status(401).send('Action not authorized')
+  }
+  next()
+}
+
+router.get('/', async (req, res) => {
   res.render('posts/posts')
 })
 
-router.get('/new', (req, res) => {
-  res.render('posts/new')
+router.get('/:id/new', checkIsOwner, async (req, res) => {
+  const id = req.params.id
+  const author = await Author.findById(id)
+  res.render('posts/new', { author: author })
 })
 
-router.post('/new', (req, res) => {
-  console.log('post created')
-  res.redirect('/posts')
+router.post('/:id/new', checkIsOwner, async (req, res) => {
+  try {
+    const id = req.params.id
+    const author = await Author.findById(id)
+    const { title, content } = req.body
+    const post = new Post({ title, content, author: author._id })
+    await post.save()
+    res.redirect(`/authors/${id}`)
+  } catch (err) {
+    res.send(err)
+  }
 })
 
 router.get('/:id', (req, res) => {
@@ -20,18 +38,18 @@ router.get('/:id', (req, res) => {
   res.render('posts/post', { id: id })
 })
 
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', checkIsOwner, (req, res) => {
   const id = req.params.id
   res.render('posts/edit', { id: id })
 })
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', checkIsOwner, (req, res) => {
   const id = req.params.id
   res.set('refresh', `2;url=http://localhost:3000/posts/${id}`)
   res.redirect(201, `/${id}`)
 })
 
-router.delete('/:id/delete', (req, res) => {
+router.delete('/:id/delete', checkIsOwner, (req, res) => {
   const id = req.params.id
   res.send(`post ${id} will be deleted here`)
 })
