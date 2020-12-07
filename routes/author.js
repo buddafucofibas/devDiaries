2ca71e9806ec28b4954357749d34433117e674af
Dorian Edwards <dorian.m.edwards@gmail.com>
2020-12-06 23:47:27 -0500

    added middleware to check if user is profile owner, completed delete author function

--- routes/author.js
+++ routes/author.js
@@ -2,12 +2,25 @@ const express = require('express')
 const router = express.Router()
 const { body, validationResult } = require('express-validator')
 const Author = require('../models/Author')
+const Post = require('../models/Post')
 // const bcrypt = require('bcrypt')
 // const saltRounds = 12
 
+// this middleware checks to see if the author's ID matches the client's ID to prevent client's with non-matching ID's from accessing data altering routes like patch, delete, etc
+const checkIsOwner = (req, res, next) => {
+  if (req.session.user_id !== req.params.id) {
+    return res.status(401).send('Action not authorized')
+  }
+  next()
+}
+
 router.get('/', async (req, res) => {
-  const authors = await Author.find()
-  res.render('authors/authors', { authors: authors })
+  try {
+    const authors = await Author.find()
+    res.render('authors/authors', { authors: authors })
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
 })
 
 // not sure whether or not to keep this, this prevents folks from trying to get /authors/new which isn't a valid request on this route.
@@ -16,21 +29,25 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
+  // check if client id matches author id and passes along this match so the page is rendered differently
   const id = req.params.id
+  const owner = req.session.user_id === id
+
   try {
     const author = await Author.findById(id)
+    const posts = await Post.find({ author: id })
     if (!author) {
       console.log('author not found')
       return res.redirect('/authors')
     }
-    res.render('authors/author', { author: author })
+    res.render('authors/author', { author: author, owner: owner, posts: posts })
   } catch (err) {
     console.error(err)
     res.redirect('/authors')
   }
 })
 
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', checkIsOwner, async (req, res) => {
   const id = req.params.id
   try {
     const author = await Author.findById(id)
@@ -48,6 +65,7 @@ router.patch(
     body('lastName').isLength({ min: 1 }).withMessage('Last name must have at least 1 character'),
     body('email').isEmail().withMessage('Please enter a valid email address'),
   ],
+  checkIsOwner,
   async (req, res) => {
     try {
       const errors = validationResult(req)
@@ -65,11 +83,14 @@ router.patch(
   }
 )
 
-router.delete('/:id/delete', async (req, res) => {
+router.delete('/:id/delete', checkIsOwner, async (req, res) => {
   const id = req.params.id
   try {
+    // deletes author from database and removes user_id from session
     await Author.findByIdAndDelete(id)
-    res.redirect('/authors')
+    await Post.deleteMany({ author: id })
+    req.session.user_id = null
+    res.redirect('/login')
   } catch (err) {
     console.error(err)
   }
