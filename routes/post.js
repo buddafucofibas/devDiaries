78adf88691fcd7ffd24077981a6b93a6efff8636
Dorian Edwards <de.pharmd2018@gmail.com>
2020-12-08 14:24:17 -0500

    added try/catch to patch request and added delete route for post

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Post = require('../models/Post')
 const Author = require('../models/Author')
+const { findByIdAndDelete } = require('../models/Post')
 const router = express.Router()
 
 const checkIsOwner = async (req, res, next) => {
@@ -58,19 +59,27 @@ router.get('/:id/edit', checkIsOwner, async (req, res) => {
 })
 
 router.patch('/:id', checkIsOwner, async (req, res) => {
-  const id = req.params.id
-  const { title, content } = req.body
-  const post = await Post.findById(id)
-  post.title = title
-  post.content = content
-  post.dateCreated = new Date()
-  await post.save()
-
-  res.redirect('/posts')
+  try {
+    const id = req.params.id
+    const { title, content } = req.body
+    const post = await Post.findById(id)
+    post.title = title
+    post.content = content
+    post.dateCreated = new Date()
+    await post.save()
+    return res.redirect('/posts')
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
 })
 
-router.delete('/:id/delete', checkIsOwner, (req, res) => {
-  const id = req.params.id
-  res.send(`post ${id} will be deleted here`)
+router.delete('/:id/delete', checkIsOwner, async (req, res) => {
+  try {
+    const id = req.params.id
+    await Post.findByIdAndDelete(id)
+    return res.redirect('/posts')
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
 })
 module.exports = router
